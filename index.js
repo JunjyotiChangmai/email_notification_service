c0117c95177f297306b2409dee602af973ff9a1b
Junjyoti Changmai <junjyotichangmai12@gmail.com>
2026-01-23 14:18:10 +0530

    updating cors

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ import { transporter } from "./services/smtp_service.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use('/', router)
