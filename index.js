3b12da1dd8ba64423ae99aa038e6960f1af6870a
Junjyoti Changmai <junjyotichangmai12@gmail.com>
2026-01-23 14:24:41 +0530

    fix

--- index.js
+++ index.js
@@ -9,12 +9,7 @@ import { transporter } from "./services/smtp_service.js";
 
 const app = express();
 
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
-app.options("*", cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use('/', router)
