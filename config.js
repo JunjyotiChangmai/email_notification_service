c30c55a5d1a0fa4ad2679f553466c290f6f901c0
Junjyoti Changmai <junjyotichangmai12@gmail.com>
2026-01-23 11:36:12 +0530

    updating

--- config.js
+++ config.js
@@ -6,3 +6,4 @@ export const SMTP_HOST = process.env.SMTP_HOST;
 export const SMTP_PORT = process.env.SMTP_PORT;
 export const SMTP_USER = process.env.SMTP_USER;
 export const SMTP_PASS = process.env.SMTP_PASS;
+export const SMTP_SECURE = process.env.SMTP_SECURE;
--- services/smtp_service.js
+++ services/smtp_service.js
@@ -1,10 +1,10 @@
 import nodemailer  from "nodemailer";
-import { SMTP_USER, SMTP_PASS, SMTP_PORT, SMTP_HOST } from "../config.js";
+import { SMTP_USER, SMTP_PASS, SMTP_PORT, SMTP_HOST, SMTP_SECURE } from "../config.js";
 
 export const transporter  = nodemailer.createTransport({
     host: SMTP_HOST,
     port: SMTP_PORT,
-    secure: false,
+    secure: SMTP_SECURE,
     auth: {
         user: SMTP_USER,
         pass: SMTP_PASS, 
